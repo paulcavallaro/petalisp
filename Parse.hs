6158d32135f0a96151c103e136f89318da089c69
Paul Cavallaro <paulcavallaro@gmail.com>
2012-04-08 17:51:03 -0400

    Initial commit, gots to implement me a lisp

---
+++ Parse.hs
@@ -0,0 +1,95 @@
+module Main (main) where
+import System.Environment
+import Control.Monad
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_-"
+
+spaces :: Parser()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             deriving (Show, Eq)
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+parseString :: Parser LispVal
+parseString = do
+    char '"'
+    x <- many ( parseEscaped <|> noneOf "\"" )
+    char '"'
+    return $ String x
+
+parseEscaped = do
+    char '\\'
+    c <- anyChar
+    case c of
+        '\\' -> return '\\'
+        'n' -> return '\n'
+        'r' -> return '\r'
+        't' -> return '\t'
+        '"' -> return '"'
+
+parseAtom :: Parser LispVal
+parseAtom = do
+          first <- letter <|> symbol
+          rest <- many (letter <|> digit <|> symbol)
+          let atom = first:rest
+          return $ case atom of
+                     "#t" -> Bool True
+                     "#f" -> Bool False
+                     _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    head <- endBy parseExpr spaces
+    tail <- char '.' >> spaces >> parseExpr
+    return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+        <|> parseString
+        <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
+
+readExpr :: String -> String
+readExpr input = case parse parseExpr "lisp" input of
+    Left err -> "No match: " ++ show err
+    Right val -> showVal val
+
+main :: IO ()
+main = do
+     args <- getArgs
+     putStrLn (readExpr (args !! 0))
